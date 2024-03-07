04348db7f2134c2c6747c5e929da5ee170f783d3
PACCAUD <anthony.paccaud@capgemini.com>
2024-03-07 17:14:13 +0100

    mise en place du StateProvider

---
+++ src/utils/StateProvider.jsx
@@ -0,0 +1,20 @@
+import { createContext, useReducer } from 'react';
+import PropTypes from 'prop-types';
+
+// Le Contexte nous permet de transmettre une prop profondément dans l’arbre des composants sans la faire passer explicitement à travers tous les composants.
+export const StateContext = createContext();
+
+// Utilise un Provider pour passer le Contexte plus bas dans l’arbre.
+// N’importe quel composant peut le lire, quelle que soit sa profondeur.
+export const StateProvider = ({children, initialState, reducer}) => (
+    // useReducer, la manière la plus simple de l’initialiser consiste à fournir l’état initial comme deuxième argument
+    <StateContext.Provider value={useReducer(reducer, initialState)}>
+        {children}
+    </StateContext.Provider>
+);
+
+StateProvider.propTypes = {
+    children: PropTypes.object,
+    initialState: PropTypes.object,
+    reducer: PropTypes.func
+};
