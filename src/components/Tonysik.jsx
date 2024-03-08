8a0b916be84339f8ed635499dc32c62b52318394
PACCAUD <anthony.paccaud@capgemini.com>
2024-03-08 14:48:05 +0100

    Mise en place du layout Tonysik

--- src/components/Tonysik.jsx
+++ src/components/Tonysik.jsx
@@ -1,7 +1,47 @@
+import styled from 'styled-components';
+import Sidebar from '@components/Sidebar';
+import Navbar from '@components/Navbar';
+import Footer from '@components/Footer';
+import Body from '@components/Body';
+
 const Tonysik = () => {
     return (
-        <div>Tonysik</div>
+        <Container>
+            <div className='tonysik_body'>
+                <Sidebar />
+                <div className='body'>
+                    <Navbar />
+                    <div className='body_contents'>
+                        <Body />
+                    </div>
+                </div>
+            </div>
+            <div className='tonysik_footer'>
+                <Footer />
+            </div>
+        </Container>
     );
 };
+
+const Container = styled.div`
+    max-width: 100vw;
+    max-height: 100vh;
+    overflow: hidden;
+    display: grid;
+    grid-template-rows: 85vh 15vh;
+.tonysik_body {
+    display: grid;
+    grid-template-columns: 15vw 85vw;
+    height: 100%;
+    width: 100%;
+    background: linear-gradient(transparent, rgba(0, 0, 0, 1));
+    background-color: rgb(32, 87, 100);
+.body {
+    height: 100%;
+    width: 100%;
+    overflow: auto;
+    }
+}
+`;
 
 export default Tonysik;
