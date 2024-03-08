505615a6333cc1f14e3de23d65d4841fb58f3944
PACCAUD <anthony.paccaud@capgemini.com>
2024-03-08 12:13:55 +0100

    Récupération du token

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
 import Login from '@components/Login';
+import { useEffect } from 'react';
 
 const App = () => {
+
+    useEffect(() => {
+
+        const hash = window.location.hash;
+        if (hash) {
+            const token = hash.substring(1).split('&')[0].split('=')[1];
+            console.log(token);
+        }
+        // Le 2e argument [] sert à dire qu'il ne doit être exécuté qu'une seule fois, après le premier rendu du composant
+    }, []);
+
     return (
         <>
             <Login/>
