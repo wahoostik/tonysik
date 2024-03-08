5b16f2a07374f84c886ace0683bb700155bb7fc7
PACCAUD <anthony.paccaud@capgemini.com>
2024-03-08 17:17:20 +0100

    Ajout de React-Icons

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 import logo from '@public/TonysikLogoWhite.png';
+import { IoLibrary } from 'react-icons/io5';
+import { MdHomeFilled, MdSearch } from 'react-icons/md';
 
 const Sidebar = () => {
     return (
@@ -8,6 +10,20 @@ const Sidebar = () => {
                 <div className="logo">
                     <img src={logo} alt="Tonysik Logo" />
                 </div>
+                <ul>
+                    <li>
+                        <MdHomeFilled />
+                        <span>Accueil</span>
+                    </li>
+                    <li>
+                        <MdSearch />
+                        <span>Rechercher</span>
+                    </li>
+                    <li>
+                        <IoLibrary />
+                        <span>Ma Librairie</span>
+                    </li>
+                </ul>
             </div>
         </Container>
     );
@@ -15,12 +31,38 @@ const Sidebar = () => {
 
 const Container = styled.div`
     background-color: black;
+    color: white;
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    width: 100%;
+.top__links {
+    display: flex;
+    flex-direction: column;
 .logo {
     text-align: center;
     margin: 1rem 0;
 img {
     max-inline-size: 80%;
     block-size: auto;
+    }
+}
+    ul {
+        list-style-type: none;
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        padding: 1rem;
+    li {
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+        cursor: pointer;
+        transition: 0.3s ease-in-out;
+        &:hover {
+            color: #a5c5ff;
+            }
+        }
     }
 }
 `;
