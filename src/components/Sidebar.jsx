975dd03a61f2f0cd87499ef2681f849867e55934
PACCAUD <anthony.paccaud@capgemini.com>
2024-03-08 16:52:31 +0100

    Début du CSS du composant Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,28 @@
 import styled from 'styled-components';
+import logo from '@public/TonysikLogoWhite.png';
 
 const Sidebar = () => {
     return (
-        <Container>Sidebar</Container>
+        <Container>
+            <div className="top__links">
+                <div className="logo">
+                    <img src={logo} alt="Tonysik Logo" />
+                </div>
+            </div>
+        </Container>
     );
 };
 
 const Container = styled.div`
     background-color: black;
-    color: white;
+.logo {
+    text-align: center;
+    margin: 1rem 0;
+img {
+    max-inline-size: 80%;
+    block-size: auto;
+    }
+}
 `;
 
 export default Sidebar;
